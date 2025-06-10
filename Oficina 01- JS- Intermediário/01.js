4b48d0d89acc930f0432452078f5e2ca57e1ffe2
Karla Castro <147751858+karla08C@users.noreply.github.com>
2025-06-10 20:52:31 -0300

    Lista de atividades

---
+++ Oficina 01- JS- Intermediário/01.js
@@ -0,0 +1,13 @@
+// 1. Crie uma função que recebe dois números e retorne o maior dos dois.
+
+function maiorNumero(num1, num2) {
+    return num1 > num2 ? num1 : num2;
+  }
+  
+  console.log(maiorNumero(15, 45));function areaCirculo(raio) {
+    return Math.PI * raio * raio;
+}
+// Exemplo de uso:
+const raio = 3;
+console.log(`A área do círculo de raio ${raio} é ${areaCirculo(raio)}`);
+ // 45
